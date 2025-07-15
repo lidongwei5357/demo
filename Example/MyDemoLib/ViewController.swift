8a5616f91b44dd8266b8c3eeaae53af5bd833210
lidongwei <lidongwei@youyueplay.com>
2025-07-15 11:20:28 +0800

    Initial commit

---
+++ Example/MyDemoLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyDemoLib
+//
+//  Created by lidongwei on 07/15/2025.
+//  Copyright (c) 2025 lidongwei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
